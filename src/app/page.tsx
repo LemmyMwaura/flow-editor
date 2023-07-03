65b94cc9465451056344aafdcf3bb1aed2b77815
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-07-03 16:42:20 +0300

    style: update layout and styles (miniupdate)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import Flow from '@/app/components/flow/Flow';
+import Flow from '@/app/components/flow/Flow'
 
 export default function Home() {
   return (
-    <main className='grid grid-cols-6'>
-      <div className='col-span-1 bg-slate-700'>
-        left - side
-      </div>
-      <Flow></Flow>
+    <main className="grid grid-cols-6">
+      <div className="col-span-1 bg-slate-700">left - side</div>
+        <Flow></Flow>
     </main>
   )
 }
